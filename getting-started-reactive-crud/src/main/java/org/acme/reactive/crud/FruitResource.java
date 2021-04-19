43d106bdbf4ea2afdfe4d46346c9c1c3a8bbc268
ths83 <ths.perron@gmail.com>
2021-04-18 20:55:46 -0400

    Add missing files

---
+++ getting-started-reactive-crud/src/main/java/org/acme/reactive/crud/FruitResource.java
@@ -0,0 +1,16 @@
+package org.acme.reactive.crud;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/fruits")
+public class FruitResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello RESTEasy Reactive";
+    }
+}
